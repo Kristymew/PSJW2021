0bee62464e4c2274b6c842b1549e4257e82d39a2
Kristymew <kristyvdberg@yahoo.com>
2021-05-11 16:38:01 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,21 +7,59 @@
 int main()
 {
 
-  
-   DDRB |= (1 << PB2) | (1 << PB6) | (1 << PB1) | (1 << PB5); // linker en rechter direction en speed op output
+  /* DDRB = 1 << PORTB1 | 1 << PORTB2 | 1 << PORTB5 | 1 << PORTB6;
+   PORTB = 1 << PORTB5;
+   _delay_ms(1000);
+   PORTB &= ~(1 << PORTB5);
    
-   _delay_ms(3000);
+   _delay_ms(1000);
+   PORTB = 1 << PB1;
+   PORTB |= 1 << PB5;
+   _delay_ms(1000);
+   PORTB &= ~(1 << PB5);
+   
+   while(1); */
+
+
+
+
+	// *** PWM *** //
+	//TCCR1A = 0b10100000; // 160
+    //TCCR1B = 0b00010001; // 17
+    //ICR1 = 400; 
+   // OCR1A = 0; // snelheid van de rechtermotor
+   // OCR1B = 0; // snelheid van de linkermotor
+   initMotors();
+
+  
+   //DDRB |= (1 << PB2) | (1 << PB6) | (1 << PB1) | (1 << PB5); // linker en rechter direction en speed op output
+   
+   _delay_ms(2000);
    
 
    while(1)
    {
+		//OCR1A = 150;
+		//OCR1B = 150;
 		
-		PORTB |= (1 << PB6) | (1 << PB5); // zet de motor links op volle kracht
-		_delay_ms(1000); // 2 sec.
-		PORTB &= ~(1 << PB6 | 1 << PB2); // zet de linker motor uit
-		_delay_ms(1800); // 1 sec
+		//PORTB |= (1 << PB6) | (1 << PB5); // zet de motor links en rechts op volle kracht
+		//_delay_ms(1000); // 
+		//PORTB &= ~(1 << PB6 | 1 << PB2); // zet de linkermotor uit
+		//OCR1B = 0; // zet de snelheid van linkermotor uit
+		//zetSnelheidMotorLinks(0);
 		
-		//PORTB |= (1 << PB6); // zet linker motor weer aan
+		//_delay_ms(500); //  
+		
+		zetSnelheidMotorsAchteruit(50);
+		
+		
+		//PORTB |= (1 << PB6) | (1 << PB5 | 1 << PB2 | 1 << PB1); // zet de motors achteruit op volle kracht
+		//_delay_ms(1800); // 2 sec.
+		//PORTB &= ~(1 << PB6 | 1 << PB2); // zet de linker motor uit
+		//_delay_ms(300); // 1 sec
+		
+		
   }
+  
 
 }
