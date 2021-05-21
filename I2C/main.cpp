adfc4c574629029e3d58c16ccc931310fd12c0a0
Stef <19027869@student.hhs.nl>
2021-05-21 13:44:30 +0200

    I2C andere sensor

--- I2C/main.cpp
+++ I2C/main.cpp
@@ -5,7 +5,7 @@
 
 #include "i2c.h"
 #include "functies.h"
-
+/*
 #define L3GD20HAdres    0b11010110 
 
 #define WHO_AM_I     	0x0F
@@ -52,16 +52,87 @@
 #define IG_DURATION     0x38 // D20H
 #define INT1_DURATION   0x38 // D20, 4200D
 #define LOW_ODR         0x39  // D20H
+*/
+#define LSM6DS33Adres    0b11010100
+
+#define FUNC_CFG_ACCESS   = 0x01
+
+#define FIFO_CTRL1        = 0x06
+#define FIFO_CTRL2        = 0x07
+#define FIFO_CTRL3        = 0x08
+#define FIFO_CTRL4        = 0x09
+#define FIFO_CTRL5        = 0x0A
+#define ORIENT_CFG_G      = 0x0B
+
+#define INT1_CTRL         = 0x0D
+#define INT2_CTRL         = 0x0E
+#define WHO_AM_I          = 0x0F
+#define CTRL1_XL          = 0x10
+#define CTRL2_G           = 0x11
+#define CTRL3_C           = 0x12
+#define CTRL4_C           = 0x13
+#define CTRL5_C           = 0x14
+#define CTRL6_C           = 0x15
+#define CTRL7_G           = 0x16
+#define CTRL8_XL          = 0x17
+#define CTRL9_XL          = 0x18
+#define CTRL10_C          = 0x19
+
+#define WAKE_UP_SRC       = 0x1B
+#define TAP_SRC           = 0x1C
+#define D6D_SRC           = 0x1D
+#define STATUS_REG        = 0x1E
+
+#define OUT_TEMP_L        = 0x20
+#define OUT_TEMP_H        = 0x21
+#define OUTX_L_G          = 0x22
+#define OUTX_H_G          = 0x23
+#define OUTY_L_G          = 0x24
+#define OUTY_H_G          = 0x25
+#define OUTZ_L_G          = 0x26
+#define OUTZ_H_G          = 0x27
+#define OUTX_L_XL         = 0x28
+#define OUTX_H_XL         = 0x29
+#define OUTY_L_XL         = 0x2A
+#define OUTY_H_XL         = 0x2B
+#define OUTZ_L_XL         = 0x2C
+#define OUTZ_H_XL         = 0x2D
+
+#define FIFO_STATUS1      = 0x3A
+#define FIFO_STATUS2      = 0x3B
+#define FIFO_STATUS3      = 0x3C
+#define FIFO_STATUS4      = 0x3D
+#define FIFO_DATA_OUT_L   = 0x3E
+#define FIFO_DATA_OUT_H   = 0x3F
+#define TIMESTAMP0_REG    = 0x40
+#define TIMESTAMP1_REG    = 0x41
+#define TIMESTAMP2_REG    = 0x42
+
+#define STEP_TIMESTAMP_L  = 0x49
+#define STEP_TIMESTAMP_H  = 0x4A
+#define STEP_COUNTER_L    = 0x4B
+#define STEP_COUNTER_H    = 0x4C
+
+#define FUNC_SRC          = 0x53
+
+#define TAP_CFG           = 0x58
+#define TAP_THS_6D        = 0x59
+#define INT_DUR2          = 0x5A
+#define WAKE_UP_THS       = 0x5B
+#define WAKE_UP_DUR       = 0x5C
+#define FREE_FALL         = 0x5D
+#define MD1_CFG           = 0x5E
+#define MD2_CFG           = 0x5F
 
 //void setConfigReg();
 //void setResolutionReg();
 void setRegister(uint8_t r);
-uint8_t readXL();
-uint8_t readXH();
-uint8_t readYL();
-uint8_t readYH();
-uint8_t readZL();
-uint8_t readZH();
+uint8_t readXLG();
+uint8_t readXHG();
+uint8_t readYLG();
+uint8_t readYHG();
+uint8_t readZLG();
+uint8_t readZHG();
 
 int main()
 {
@@ -79,25 +150,25 @@ int main()
         //_delay_ms(750);
 		writeString("Positie is: ");
 		
-		setRegister(OUT_X_L);
-		uint8_t XL=readXL();
-		setRegister(OUT_X_H);
-		uint8_t XH=readXH();
-		uint16_t X = (XH << 8 | XL);
+		setRegister(OUTX_L_G);
+		uint8_t XLG = readXLG();
+		setRegister(OUTX_H_G);
+		uint8_t XHG = readXHG();
+		uint16_t X = (XHG << 8 | XLG);
 		writeUnt(X);
 		
-		setRegister(OUT_Y_L);
-		uint8_t YL=readYL();
-		setRegister(OUT_Y_H);
-		uint8_t YH=readYH();
-		uint16_t Y = (YH << 8 | YL);
+		setRegister(OUTY_L_G);
+		uint8_t YLG = readYLG();
+		setRegister(OUTY_H_G);
+		uint8_t YHG = readYHG();
+		uint16_t Y = (YHG << 8 | YLG);
 		writeUnt(Y);
 		
-		setRegister(OUT_Z_L);
-		uint8_t ZL=readZL();
-		setRegister(OUT_Z_H);
-		uint8_t ZH=readZH();
-		uint16_t Z = (ZH << 8 | ZL);
+		setRegister(OUTZ_L_G);
+		uint8_t ZLG = readZLG();
+		setRegister(OUTZ_H_G);
+		uint8_t ZHG = readZHG();
+		uint16_t Z = (ZHG << 8 | ZLG);
 		writeUnt(Z);
 		
 		writeString("\n\r");
@@ -126,11 +197,11 @@ int main()
 
 void setRegister(uint8_t r) {
 	i2cStart();
-	i2cSend(L3GD20HAdres<<1);   //i2c adres master schrijf
+	i2cSend(LSM6DS33Adres<<1);   //i2c adres master schrijf
 	i2cSend(r);
 	i2cStop();
 }
-
+/*
 uint8_t readXL() {
 	i2cStart();
 	i2cSend((L3GD20HAdres<<1)+1);   //i2c adres master read
@@ -170,6 +241,50 @@ uint8_t readZL() {
 uint8_t readZH() {
 	i2cStart();
 	i2cSend((L3GD20HAdres<<1)+1);   //i2c adres master read
+	uint8_t zhg = i2cReadNoAck();	
+	i2cStop();
+	return zhg;
+}
+*/
+uint8_t readXLG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);   //i2c adres master read
+	uint8_t xlg = i2cReadNoAck();	
+	i2cStop();
+	return xlg;
+}
+uint8_t readXHG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);   //i2c adres master read
+	uint8_t xhg = i2cReadNoAck();	
+	i2cStop();
+	return xhg;
+}
+
+uint8_t readYLG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);	 //i2c adres master read
+	uint8_t ylg = i2cReadNoAck();
+	i2cStop();
+	return ylg;
+}
+uint8_t readYHG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);   //i2c adres master read
+	uint8_t yhg = i2cReadNoAck();	
+	i2cStop();
+	return yhg;
+}
+uint8_t readZLG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);   //i2c adres master read
+	uint8_t zlg = i2cReadNoAck();
+	i2cStop();
+	return zlg;
+}
+uint8_t readZHG() {
+	i2cStart();
+	i2cSend((LSM6DS33Adres<<1)+1);   //i2c adres master read
 	uint8_t zhg = i2cReadNoAck();	
 	i2cStop();
 	return zhg;
